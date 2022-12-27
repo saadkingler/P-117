86ff8c183f373e4ae2109124b7f1e8d9bd5564e9
saadkingler <120109737+saadkingler@users.noreply.github.com>
2022-12-27 17:34:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+   canvas = createCanvas(300,300);
+   canvas.center();
+   background("white");
+}
+
+function clear_canvas()
+{
+    background("white");
+}
+
+function draw()
+{
+
+}
